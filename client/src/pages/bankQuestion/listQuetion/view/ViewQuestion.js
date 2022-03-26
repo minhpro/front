391da7f37c230609667b1a8da72d8c83abf90e47
SauLuoi <dai.dotien@gmail.com>
2022-03-26 18:52:41 +0700

    cau hoi

--- client/src/pages/bankQuestion/listQuetion/view/ViewQuestion.js
+++ client/src/pages/bankQuestion/listQuetion/view/ViewQuestion.js
@@ -24,10 +24,43 @@ export const ViewQuestion = (id) => {
   const Question = () => {
     return (
       <Mui.Stack>
+        <InforQuestion data={data} />
         <p>Loại câu hỏi: {getType(data.type)}</p>
+        <p> Ten cau hoi: {data.name}</p>
+        <p>ID: {data.id}</p>
+        <p>thoi gian lam bai: {data.time}</p>
+        <p>Cau hoi</p>
+
+        <div dangerouslySetInnerHTML={{ __html: data.question }} />
+        <p>Dap an Dung</p>
+        <div dangerouslySetInnerHTML={{ __html: data.answerOne }} />
+
+        <p>Huong dan giai</p>
+        <div dangerouslySetInnerHTML={{ __html: data.suggest }} />
+        <p>Huong dan giai</p>
+        <div dangerouslySetInnerHTML={{ __html: data.suggest }} />
       </Mui.Stack>
     );
   };
 
   return <Mui.Stack>{data ? <Question /> : <div> load</div>}</Mui.Stack>;
+};
+
+const InforQuestion = ({ data }) => {
+  return (
+    <Mui.Grid container>
+      <Mui.Grid item xs={6} border={"solid 1px"}>
+        <p>Lớp: {data.unitData?.chapterData?.subjectData?.classs?.name}</p>
+      </Mui.Grid>
+      <Mui.Grid item xs={6} border={"solid 1px"}>
+        <p>Mon: {data.unitData?.chapterData?.subjectData?.name}</p>
+      </Mui.Grid>
+      <Mui.Grid item xs={12} border={"solid 1px"}>
+        <p>Chuong: {data.unitData?.chapterData?.name}</p>
+      </Mui.Grid>
+      <Mui.Grid item xs={12} border={"solid 1px"}>
+        <p>Bai: {data.unitData?.name}</p>
+      </Mui.Grid>
+    </Mui.Grid>
+  );
 };
