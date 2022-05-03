81ad9aec1a1824a2cd0b998758297bec48e80cfc
thanhtv32 <thanhtv32.fsoft@gmail.com>
2022-05-03 11:34:06 +0700

    edit link

--- client/src/assets/contants/apiContant.js
+++ client/src/assets/contants/apiContant.js
@@ -2,8 +2,8 @@
 
 // export const apiNofi = "http://18.179.5.86:8080/api/event/register";
 
-export const apiUrl = "http://localhost:8081/api/";
+export const apiUrl = "http://115.146.127.51:8080/api/";
 
 export const apiNofi = "http://18.179.5.86:8080/api/event/register";
 
-export const lmsHomeUrl = "http://localhost:3000";
+export const lmsHomeUrl = "https://ebd.actgroup.com.vn";
